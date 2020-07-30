391ab65aa992348333a19280b97774f4987d5334
TimOcampo <timocmpo@TimOcampos-MacBook-Pro.local>
2020-07-30 14:51:22 -0700

    master | configure package.json for heroku deployment, changed public to build in server.js .

--- server/server.js
+++ server/server.js
@@ -26,9 +26,9 @@ app.use('/api/skills', skills)
 // app.get('/', (req, res) => res.send('Welcome to the back end of my portfolio!'));
 //Serves static assets if in prod
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('public'));
+    app.use(express.static('build'));
     app.get('*', (req, res)=>{
-        res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+        res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
     })
 }
 
