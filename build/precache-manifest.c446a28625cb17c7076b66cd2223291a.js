3f238e0652610d46016e97f42b42fe7cf8ab8850
TimOcampo <timocmpo@TimOcampos-MacBook-Pro.local>
2020-07-30 18:43:47 -0700

    master | added build folder.

---
+++ build/precache-manifest.c446a28625cb17c7076b66cd2223291a.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "503780eb722d87cb537ac97d23c56d3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9781117c2f96fe95b9fa",
+    "url": "/static/css/2.e2dd9b0a.chunk.css"
+  },
+  {
+    "revision": "92c97f3c4755d28c6540",
+    "url": "/static/css/main.402294d9.chunk.css"
+  },
+  {
+    "revision": "9781117c2f96fe95b9fa",
+    "url": "/static/js/2.dddd95c8.chunk.js"
+  },
+  {
+    "revision": "4bf00ea42f5dad8ab33d5f32afec9baf",
+    "url": "/static/js/2.dddd95c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92c97f3c4755d28c6540",
+    "url": "/static/js/main.afa10322.chunk.js"
+  },
+  {
+    "revision": "2faf43ff99bb4578d2c8",
+    "url": "/static/js/runtime-main.df564cd3.js"
+  },
+  {
+    "revision": "9df136e7770d13d523c5fb243f6a21d3",
+    "url": "/static/media/D-DIN-Bold.9df136e7.ttf"
+  },
+  {
+    "revision": "cc50fed080ebc3dd10d88569e93ef73c",
+    "url": "/static/media/D-DIN-Italic.cc50fed0.ttf"
+  },
+  {
+    "revision": "b6c5945add1318c22fa12bd890d6b6f2",
+    "url": "/static/media/D-DIN.b6c5945a.ttf"
+  },
+  {
+    "revision": "dfdd799b07dbb317b4b2f0870d03509a",
+    "url": "/static/media/D-DINCondensed-Bold.dfdd799b.ttf"
+  },
+  {
+    "revision": "37408c7b0896c2841258210a4cdc448f",
+    "url": "/static/media/D-DINCondensed.37408c7b.ttf"
+  },
+  {
+    "revision": "d455efd4cb5c4047f287cb7a8b6ebe37",
+    "url": "/static/media/D-DINExp-Bold.d455efd4.ttf"
+  },
+  {
+    "revision": "ee57ed3e6283e25d4d79ba4700c925bb",
+    "url": "/static/media/D-DINExp-Italic.ee57ed3e.ttf"
+  },
+  {
+    "revision": "5f4a741245c4080e33150f8fe6b6238a",
+    "url": "/static/media/D-DINExp.5f4a7412.ttf"
+  },
+  {
+    "revision": "89c3d72ecbb5308e03321bd8dc02ba00",
+    "url": "/static/media/MonumentExtended-Regular.89c3d72e.ttf"
+  },
+  {
+    "revision": "f8f6ef582e122916a01d6b1105dd1aab",
+    "url": "/static/media/MonumentExtended-Ultrabold.f8f6ef58.ttf"
+  },
+  {
+    "revision": "be6c257dfdb2c1395c962e8829d27e27",
+    "url": "/static/media/Purple-technology-data-internet-72 copy.be6c257d.jpg"
+  },
+  {
+    "revision": "21f36981670b7817124f77cd451138ec",
+    "url": "/static/media/commerce-and-shopping.21f36981.svg"
+  },
+  {
+    "revision": "a411e067f91f9cb16a299366cdf362ad",
+    "url": "/static/media/dev-connector.a411e067.png"
+  },
+  {
+    "revision": "31d5f96b1a7fe23762cfcd5ab807dd2f",
+    "url": "/static/media/devconnector-mobile.31d5f96b.png"
+  },
+  {
+    "revision": "ed1736f7a0968bbdef350d2df07d4035",
+    "url": "/static/media/doms-noms-tile.ed1736f7.png"
+  },
+  {
+    "revision": "be8a9f9640e1f96ee0bb0188cf4501fa",
+    "url": "/static/media/facebook (2).be8a9f96.svg"
+  },
+  {
+    "revision": "281c35b2d6e1107560964d7e02314296",
+    "url": "/static/media/github.281c35b2.svg"
+  },
+  {
+    "revision": "15c157e1604ba781204712e884e87d9b",
+    "url": "/static/media/landing-page.15c157e1.png"
+  },
+  {
+    "revision": "e2b086493f469ad7e07dd6064f4ba5e3",
+    "url": "/static/media/linkedin (3).e2b08649.svg"
+  },
+  {
+    "revision": "fb13e52510d395caa7653eaf50b848e2",
+    "url": "/static/media/max-headroom.fb13e525.png"
+  },
+  {
+    "revision": "0632b2fb8015b0a65c11428706023915",
+    "url": "/static/media/multimedia.0632b2fb.svg"
+  },
+  {
+    "revision": "4abfaab3e0dce6e7c091efde992645cc",
+    "url": "/static/media/noisy-texture-blk.4abfaab3.png"
+  },
+  {
+    "revision": "6f2ffec5caeb0cb159c097cff73fb423",
+    "url": "/static/media/phone.6f2ffec5.svg"
+  },
+  {
+    "revision": "c6cfb8bd5cdef2f138945d185e96c1b6",
+    "url": "/static/media/placeholder.c6cfb8bd.svg"
+  },
+  {
+    "revision": "2b3bb75c866501aaf18afccdbd88a2dd",
+    "url": "/static/media/quarter-circle.2b3bb75c.svg"
+  },
+  {
+    "revision": "8ccba9f73d82ad7334528162ff97d64c",
+    "url": "/static/media/tim-01-stencil-transparet.8ccba9f7.png"
+  },
+  {
+    "revision": "99eb8fd020c0e035e80634442df8ffe4",
+    "url": "/static/media/twitter (1).99eb8fd0.svg"
+  }
+]);
